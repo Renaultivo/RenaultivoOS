87b554142de5f234de48a0cb21d3d15ebc3261db
Renault Scabora <renaultscabora@hotmail.com>
2020-11-14 08:07:29 -0300

    Update kernel and create run script

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -1,16 +1,41 @@
 [BITS 16]
 [ORG 0X7E00]
 
-call jumpLine
-mov bp, msg
-mov cx, msgLen
-call printMessage
+start:
+  mov [bootDrive], dl
+  call jumpLine
+  mov bp, msg
+  mov cx, msgLen
+  call printMessage
+
+checkCPU:
+  mov eax, 0x80000000
+  cpuid
+  cmp eax, 0x80000001
+  jb notSupported
+
+  mov eax, 0x80000001
+  cpuid
+  test edx, (1<<29)
+  jz notSupported
+  
+  test edx, (1<<26)
+  jz notSupported 
 
 main:
   hlt
   jmp $
 
+notSupported:
+  mov bp, notSupportedMessage
+  mov cx, notSupportedMessageLen
+  call printMessage
+  jmp main 
+
 %include "libs/bios/printMessage.asm"
 
+bootDrive db 0
 msg db 'Kernel loaded'
 msgLen equ $-msg
+notSupportedMessage db 'Long mode not supported'
+notSupportedMessageLen equ $-notSupportedMessage
